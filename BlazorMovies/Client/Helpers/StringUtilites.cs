65fb4c1411b53f656de6a0480818369cb0fb8699
Paras Rawat <parasrawat67@gmail.com>
2021-09-04 15:16:02 +0530

    Using Class File With Razor And using Imports.Razor To avoid using keryword in Razor

---
+++ BlazorMovies/Client/Helpers/StringUtilites.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilites
+    {
+        public static string CustomerToLower(string Value) => Value.ToLower();
+    }
+}
